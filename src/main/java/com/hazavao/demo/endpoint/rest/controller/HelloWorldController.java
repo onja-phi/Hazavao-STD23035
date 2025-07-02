dbe3ddabd3f10cd81eb569a4c6720b81457efc5d
onja-phi <hei.onja.2@gmail.com>
2025-07-02 08:59:17 +0300

    feat: add HelloWorldController

---
+++ src/main/java/com/hazavao/demo/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.hazavao.demo.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "... world!";
+  }
+}
